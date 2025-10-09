f81732126aee0fef2e1dd14412eeb4d99b313698
Mikreselasie <abiymikreselasie21@gmail.com>
2025-10-09 15:33:51 +0300

    The last push is ready

--- mobile/lib/features/home/news/presentation/bloc/news_event.dart
+++
@@ -1,9 +0,0 @@
-import '../../domain/usecases/get_news_updates.dart';
-
-abstract class NewsEvent {}
-
-class FetchNewsUpdates extends NewsEvent {
-  final NewsCategory category;
-
-  FetchNewsUpdates({this.category = NewsCategory.all});
-}
